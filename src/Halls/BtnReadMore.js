bd1b84141042098712ea6f10bb35582010ada9ea
Mohamed Ayman Awara <137450654+Mohamed-A-Awara@users.noreply.github.com>
2024-03-04 10:19:39 -0800

    Add files via upload

---
+++ src/Halls/BtnReadMore.js
@@ -0,0 +1,36 @@
+import Accordion from 'react-bootstrap/Accordion';
+import { useAccordionButton } from 'react-bootstrap/AccordionButton';
+import './Halls.css'
+
+
+
+function ContextAwareToggle({ children, eventKey, callback }) {
+
+    const decoratedOnClick = useAccordionButton(
+        eventKey,
+        () => callback && callback(eventKey),
+    );
+
+    return (
+        <button
+        type="button"
+        onClick={decoratedOnClick}
+        className='btn-readMore'
+        >
+        {children}
+        </button>
+    );
+}
+
+function BtnReadMore(props) {
+    return (
+        <Accordion defaultActiveKey="0">
+            <ContextAwareToggle eventKey="1">Read More !</ContextAwareToggle>
+            <Accordion.Collapse eventKey="1">
+                <p>{props.content}</p>
+            </Accordion.Collapse>
+        </Accordion>
+    );
+}
+
+export default BtnReadMore
