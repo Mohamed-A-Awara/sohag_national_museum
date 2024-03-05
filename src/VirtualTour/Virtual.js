6c47c74e74f35e776bf1f3821df5c4ae1602e6b9
Mohamed Ayman Awara <137450654+Mohamed-A-Awara@users.noreply.github.com>
2024-03-05 15:02:55 -0800

    Update Virtual.js

--- src/VirtualTour/Virtual.js
+++ src/VirtualTour/Virtual.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import Main_Header from './Main-Header'
+// import Lab_View from './Lab-View'
+import {  Container } from 'react-bootstrap'
+import Mobile_View from './Mobile-View'
+import Lab_View from './Lab-View'
+import Footer from '../Footer/Footer'
 
 function Virtual() {
+
+
+
     return (
-        <>Virtual</>
+        <>
+            <Main_Header /> 
+
+            <section className='tour-guide mt-5'>
+                <Container>
+                    <h2>Learn about some of the museum's pieces</h2>
+                </Container>
+            </section>
+            
+            <section>
+                {ViewTour()}
+            </section>
+            <Footer/>
+        </>
     )
 }
 
+function ViewTour (){
+    if (window.screen.availWidth <= 768){
+        return <Mobile_View />
+    }
+    else {
+        return <Lab_View />
+    }
+}
 export default Virtual
