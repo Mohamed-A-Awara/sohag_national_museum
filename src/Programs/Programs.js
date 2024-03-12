3f7fbe213b5f94dca263ef4286179cbad9d8520f
Amira mohamed <103863618+AMIRAMOHAMED44@users.noreply.github.com>
2024-03-12 23:19:17 +0200

    Update Programs.js

--- src/Programs/Programs.js
+++ src/Programs/Programs.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { Image } from 'react-bootstrap'
 import Footer from '../Footer/Footer'
+import BtnReadMore from '../Halls/BtnReadMore'
 import './Programs.css'
+import '../Halls/BtnReadMore.css'
 import img1 from'../Tools/Imgs/img1_programs .png'
 import img2 from '../Tools/Imgs/img2_programs .png'
 import img3 from '../Tools/Imgs/img3_programs .png'
@@ -50,34 +53,35 @@ function Programs() {
             <section className='sec2'>
                 <div className='top'>
                     <div className='text'>
-                        <p>Implementing the museum sector’s plan aimed at holding temporary annual exhibitions in each museum</p>
-                        <p>Renewing the museum display through these temporary exhibitions to provide a renewed view of the museum to attract visitors. It also serves as a window for the stored artifacts to see the light through these exhibitions,
-                                as well as linking the artifacts to national events and local and international occasions.</p>
+                        <p>Implementing the museum sector’s plan aimed at holding temporary annual exhibitions in each museum
+                        <BtnReadMore content="Renewing the museum display through these temporary exhibitions to provide a renewed view of the museum to attract visitors. It also serves as a window for the stored artifacts to see the light through these exhibitions,
+                                as well as linking the artifacts to national events and local and international occasions." /></p>
+                        
                     </div>
                     <div className='image'>
-                        <img src={img1}/>
+                        <Image src={img1}/>
                     </div>
                 </div>
                 {/* ////////////////////////////////////////////////////////////////// */}
                 <div className='mid'>
                 <div className='text'>
                     <p>Continuous training for museum curators on museum display, writing archaeological and historical proposals,
-                            display cards, and selecting artifacts that serve the topics.</p>
-                    <p>Informing visitors that our antiquities are the subject of the world’s admiration and talk, 
-                        and it is our duty to preserve them in order to hand them over to those after us, just as we received them before us.</p>
+                            display cards, and selecting artifacts that serve the topics.
+                            <BtnReadMore content="Informing visitors that our antiquities are the subject of the world’s admiration and talk, 
+                        and it is our duty to preserve them in order to hand them over to those after us, just as we received them before us."/></p>
                 </div>
                     <div className='image'>
-                    <img src={img2}/>
+                    <Image src={img2}/>
                     </div>
                 </div>
                 {/* ////////////////////////////////////////////////////////////////// */}
                 <div className='bottom'>
                 <div className='text'>
-                    <p>Maximizing the societal role of the museum through these exhibitions to increase archaeological awareness and introduce cultural heritage through seminars, lectures and workshops 
-                        that will be held on the sidelines of the exhibition during the three-month life of the exhibition.</p>
+                    <p>Maximizing the societal role of the museum through these exhibitions to increase archaeological awareness 
+                    <BtnReadMore content= "and introduce cultural heritage through seminars, lectures and workshops that will be held on the sidelines of the exhibition during the three-month life of the exhibition."/></p>
                 </div>
                     <div className='image'>
-                    <img src={img3}/>
+                    <Image src={img3}/>
                     </div>
                 </div>
             </section>
@@ -86,57 +90,57 @@ function Programs() {
                     <p>Educational Department Activities</p>
         </div>
         <div className='text-before-sec'>
-            <p className='sec1_p1'>Highlighting the role of museums as enlightening and educational institutions by linking the museum to society in all its categories and by participating in all local and international events.</p>
-            <p className='sec1_p2'>This is done by participating in a group of technical and educational workshops and educational lectures for all groups, students of all educational levels, and university students.</p>
+            <p className='sec1_p1'>Highlighting the role of museums as enlightening and educational institutions by linking the museum to society in all its categories and by participating in all local and international events.
+            <BtnReadMore content= "This is done by participating in a group of technical and educational workshops and educational lectures for all groups, students of all educational levels, and university students."/></p>
         </div>
         <section className='sec3'>
             <div className='sec3_div'>
-                <img src={img4}/>
+                <Image src={img4}/>
                 <p>Celebrating Antiquities Day</p>
             </div>
             <div className='sec3_div'>
-                <img src={img5}/>
+                <Image src={img5}/>
                 <p>Celebrating World Cancer Day</p>
             </div>
             <div className='sec3_div'>
-                <img src={img6}/>
+                <Image src={img6}/>
                 <p>An individual drawing workshop for </p>
                 <p>one of the gifted children</p>
             </div>
             <div className='sec3_div'>
-                <img src={img7}/>
+                <Image src={img7}/>
                 <p>Jewelry and beadwork workshop</p>
             </div>
             <div className='sec3_div'>
-                <img src={img8}/>
+                <Image src={img8}/>
                 <p>Celebrating Mother's Day</p>
             </div>
             <div className='sec3_div'>
-                <img src={img9}/>
+                <Image src={img9}/>
                 <p>Coloring workshop for children on the occasion of International Museum Day</p>
             </div>
             <div className='sec3_div'>
-                <img src={img10}/>
+                <Image src={img10}/>
                 <p>Mawlid al-Nabawi workshop</p>
             </div>
             <div className='sec3_div'>
-                <img src={img11}/>
+                <Image src={img11}/>
                 <p>A workshop for making Eid sheep figures</p>
             </div>
             <div className='sec3_div'>
-                <img src={img12}/>
+                <Image src={img12}/>
                 <p>Heritage day lectures</p>
             </div>
             <div className='sec3_div'>
-                <img src={img13}/>
+                <Image src={img13}/>
                 <p>Teaching Hajj rituals</p>
             </div>
             <div className='sec3_div'>
-                <img src={img14}/>
+                <Image src={img14}/>
                 <p>Workshop on teaching hieroglyphic writing</p>
             </div>
             <div className='sec3_div'>
-                <img src={img15}/>
+                <Image src={img15}/>
                 <p>An artistic segment and a historical presentation in Pharaonic costume</p>
             </div>
             
@@ -147,23 +151,23 @@ function Programs() {
         </div>
         <div className='text-before-sec'>
             <p className='sec1_p1'>Many workshops, entertainment activities and lectures were held
-                    Through it, people of determination are integrated into the museum,and attention is paid to providing all means of safety. Special badges have been placed and the museum is prepared to receive them.</p>
+                    Through it<BtnReadMore content= ", people of determination are integrated into the museum,and attention is paid to providing all means of safety. Special badges have been placed and the museum is prepared to receive them."/></p>
         </div>
         <section className='sec4'>
             <div className='sec4_div'>
-                    <img src={img16}/>
+                    <Image src={img16}/>
                     <p>A lecture on the rights of people with disabilities in ancient Egypt</p>
             </div>
             <div className='sec4_div'>
-                    <img src={img17}/>
+                    <Image src={img17}/>
                     <p> Celebrating World Braille Language Day</p>
             </div>
             <div className='sec4_div'>
-                <img src={img18}/>
+                <Image src={img18}/>
                 <p>Humanitarian Action Day </p>
             </div>
             <div className='sec4_div'>
-                <img src={img19}/>
+                <Image src={img19}/>
                 <p>Sign language workshop </p>
             </div>
         </section>
@@ -172,30 +176,28 @@ function Programs() {
             <p>Training</p>
         </div>
         <div className='text-before-sec'>
-            <p className='sec1_p1'>Providing training programs for university students in the departments of Egyptian antiquities, Islamic antiquities, and restoration departments, to introduce the role of museums in society, to increase archaeological awareness, to introduce museum management and management of museum collections, to give trainees an idea of the role and importance of the museum in society, the origin and development of museums, and an explanation of museum holdings, management of museum collections, temporary exhibitions, the role of museum trustees, and tours. 
-            Internal and external, as well as the role of the restoration department</p>
-            <p className='sec1_p2'>As well as lectures for archaeological field workers from various sectors.</p>
+            <p className='sec1_p1'>Providing training programs for university students in the departments of Egyptian antiquities<BtnReadMore content= ", Islamic antiquities, and restoration departments, to introduce the role of museums in society, to increase archaeological awareness, to introduce museum management and management of museum collections, to give trainees an idea of the role and importance of the museum in society, the origin and development of museums, and an explanation of museum holdings, management of museum collections, temporary exhibitions, the role of museum trustees, and tours.Internal and external, as well as the role of the restoration department As well as lectures for archaeological field workers from various sectors."/></p>
         </div>
         <section className='sec5'>
             <div className='sec5_div1'> 
-                <img src={img20}/>
+                <Image src={img20}/>
                 <p id='p1'>An educational symposium (a series of lectures entitled Museums and archaeological sites between reality and aspiration)</p>
                 <p>In partnership with the Luxor Museum of Ancient Art</p>
             </div>
             <div className='sec5_div1'>
-                <img src={img21}/>
+                <Image src={img21}/>
                 <p>Summer training for fine arts students (Restoration Department)</p>
             </div>
             <div className='sec5_div2'>
-            <img src={img22}/>
-            <img src={img23}/>
+            <Image src={img22}/>
+            <Image src={img23}/>
             </div>
             <div className='sec5_div3'>
                 <p>Training for students of Egyptian and Islamic antiquities</p>
             </div>
             <div className='sec5_div2'>
-            <img src={img24}/>
-            <img src={img25}/>
+            <Image src={img24}/>
+            <Image src={img25}/>
             </div>
             <div className='sec5_div3'>
                 <p>Training for students of Egyptian antiquities</p>
@@ -207,27 +209,25 @@ function Programs() {
         </div>
         <section className='sec11'>
             <div className='sec11_div'>
-                <img src={img26}/>
+                <Image src={img26}/>
                 <p>Advisor to the Minister of Tourism</p>
             </div>
             <div className='sec11_div'>
-            <img src={img27}/>
+            <Image src={img27}/>
                 <p>Italian Ambassador</p>
             </div>
             <div className='sec11_div'>
-            <img src={img28}/>
+            <Image src={img28}/>
                 <p>Former director of the Berlin Museum and former director of the Munich Museum</p>
             </div>
             <div className='sec11_div'>
-            <img src={img29}/>
+            <Image src={img29}/>
                 <p>Director of the French Institute of Oriental Archaeology</p>
             </div>
-
         </section>
+        {Footer}
         </>
     )
 }
 
 export default Programs
-
-
