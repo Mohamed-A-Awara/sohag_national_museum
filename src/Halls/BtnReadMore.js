ba1d7567a88c558c17d7e91f24f8f101ba551e53
Amira mohamed <103863618+AMIRAMOHAMED44@users.noreply.github.com>
2024-03-13 14:34:00 +0200

    Update BtnReadMore.js

--- src/Halls/BtnReadMore.js
+++ src/Halls/BtnReadMore.js
@@ -1,38 +1,35 @@
 import Accordion from 'react-bootstrap/Accordion';
 import { useAccordionButton } from 'react-bootstrap/AccordionButton';
-import './Halls.css'
-import BtnReadMore from './BtnReadMore'
-import './BtnReadMore.css'
-
-
+import './BtnReadMore.css';
+import { useState } from 'react';
 
 function ContextAwareToggle({ children, eventKey, callback }) {
-
-    const decoratedOnClick = useAccordionButton(
-        eventKey,
-        () => callback && callback(eventKey),
-    );
+    const decoratedOnClick = useAccordionButton(eventKey, () => callback && callback(eventKey));
 
     return (
-        <button
-        type="button"
-        onClick={decoratedOnClick}
-        className='btn-readMore'
-        >
+        <button type="button" onClick={decoratedOnClick} className='btn-readMore'>
         {children}
         </button>
     );
 }
 
 function BtnReadMore(props) {
+    const [showFullContent, setShowFullContent] = useState(false);
+
+    const toggleContent = () => {
+        setShowFullContent(!showFullContent);
+    };
+
     return (
         <Accordion defaultActiveKey="0">
-            <ContextAwareToggle eventKey="1">Read More !</ContextAwareToggle>
-            <Accordion.Collapse eventKey="1">
-                <p>{props.content}</p>
-            </Accordion.Collapse>
+        <ContextAwareToggle eventKey="1" callback={toggleContent}>
+            {showFullContent ? 'Read Less !' : 'Read More !'}
+        </ContextAwareToggle>
+        <Accordion.Collapse eventKey="1">
+            <p>{showFullContent ? props.content : props.content.substring(0, 100)}</p>
+        </Accordion.Collapse>
         </Accordion>
     );
-}
+    }
 
-export default BtnReadMore
+export default BtnReadMore;
