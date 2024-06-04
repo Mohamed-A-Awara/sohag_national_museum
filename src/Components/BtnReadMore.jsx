2c67d429f1eef4a277f1e6fec6284145cf6b5fd4
Mohamed Ayman Awara <137450654+Mohamed-A-Awara@users.noreply.github.com>
2024-06-04 11:10:28 -0700

    Update BtnReadMore.jsx

--- src/Components/BtnReadMore.jsx
+++ src/Components/BtnReadMore.jsx
@@ -3,6 +3,7 @@ import { useAccordionButton } from 'react-bootstrap/AccordionButton';
 import '../Styles/BtnReadMore.Styled.js';
 import { useState } from 'react';
 import { ReadMore } from '../Styles/BtnReadMore.Styled.js';
+import { useTranslation } from 'react-i18next';
 
 function ContextAwareToggle({ children, eventKey, callback }) {
     const decoratedOnClick = useAccordionButton(eventKey, () => callback && callback(eventKey));
@@ -16,6 +17,7 @@ function ContextAwareToggle({ children, eventKey, callback }) {
 
 
 function BtnReadMore(props) {
+    const {t} = useTranslation()
     const [showFullContent, setShowFullContent] = useState(false);
 
     const toggleContent = () => {
@@ -25,7 +27,7 @@ function BtnReadMore(props) {
     return (
         <Accordion defaultActiveKey="0" style={{display : "inline" , marginLeft : "10px"}}>
         <ContextAwareToggle eventKey="1" callback={toggleContent}>
-            {showFullContent ? 'Read Less !' : 'Read More !'}
+            {showFullContent ? `${t('ReadLess')} !` : `${t('btnLearn')} !`  }
         </ContextAwareToggle>
         <Accordion.Collapse eventKey="1">
             <p>{showFullContent ? props.content : props.content.substring(0, 100)}</p>
